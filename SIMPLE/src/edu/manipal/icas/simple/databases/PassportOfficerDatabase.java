d7b1753bdd46cf7607b73c352b1dbcac0abe0a55
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-10-19 22:16:09 +0530

    feat: Add operations and column names to PassportOfficerDatabase

---
+++ SIMPLE/src/edu/manipal/icas/simple/databases/PassportOfficerDatabase.java
@@ -0,0 +1,40 @@
+package edu.manipal.icas.simple.databases;
+
+import java.io.IOException;
+
+import edu.manipal.icas.simple.models.PassportOfficerRole;
+
+/**
+ * Interface that defines operations to be implemented in an API that persists
+ * data about passport officers.
+ * 
+ * @author Vishwas Adiga (vishwas.adiga@learner.manipal.edu)
+ *
+ */
+public interface PassportOfficerDatabase {
+	/**
+	 * Column representing a unique ID for each passport officer. To be used as the
+	 * primary key.
+	 */
+	static final String FIELD_ID = "ID";
+
+	/** Column that holds the role played by a given officer. */
+	static final String FIELD_OFFICER_ROLE = "ROLE";
+
+	/**
+	 * Checks whether a passport officer of a given ID exists in the database.
+	 * 
+	 * @param officerId ID of the officer
+	 * @return {@code true} if the entry exists, {@code false} otherwise
+	 */
+	Boolean passportOfficerExists(Integer officerId);
+
+	/**
+	 * Fetches the role played by a given passport officer from the database.
+	 * 
+	 * @param officerId ID of the officer whose role is to be obtained
+	 * @return one of {@link edu.manipal.icas.simple.models.PassportOfficerRole}
+	 * @throws IOException if no passport officer was found for the passed ID
+	 */
+	PassportOfficerRole fetchOfficerRole(Integer officerId) throws IOException;
+}
