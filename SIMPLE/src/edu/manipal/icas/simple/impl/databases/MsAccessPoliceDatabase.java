814a0c556eb6dbac90cbf37d111bbec9c481a41f
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-10-19 22:26:59 +0530

    feat: Implement PoliceDatabase

---
+++ SIMPLE/src/edu/manipal/icas/simple/impl/databases/MsAccessPoliceDatabase.java
@@ -0,0 +1,28 @@
+package edu.manipal.icas.simple.impl.databases;
+
+import edu.manipal.icas.simple.databases.PoliceDatabase;
+
+/**
+ * Concrete class that implements persistence API operations from
+ * {@link PoliceDatabase}
+ * 
+ * @author Vishwas Adiga (vishwas.adiga@learner.manipal.edu)
+ *
+ */
+public class MsAccessPoliceDatabase extends MsAccessDatabase implements PoliceDatabase {
+	private static final MsAccessPoliceDatabase DATABASE = new MsAccessPoliceDatabase();
+
+	private MsAccessPoliceDatabase() {
+		super("Police");
+	}
+
+	public static MsAccessPoliceDatabase geDatabase() {
+		return DATABASE;
+	}
+
+	@Override
+	public Boolean policeExists(Integer badgeId) {
+		return rowExists(badgeId);
+	}
+
+}
