2a74ca410a6ba193ae764b9fc2810021e57ffcc1
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-10-25 14:36:55 +0530

    Revert "temp: Test view in main"
    This reverts commit d6c5624f52f02f175f655952537a21415526c6b6.

--- SIMPLE/src/edu/manipal/icas/simple/Main.java
+++ SIMPLE/src/edu/manipal/icas/simple/Main.java
@@ -1,15 +1,10 @@
 package edu.manipal.icas.simple;
 
-import edu.manipal.icas.simple.impl.views.CitizenLoginViewImpl;
-import edu.manipal.icas.simple.views.CitizenLoginView;
-
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("SIMPLE");
 
-		CitizenLoginView view = new CitizenLoginViewImpl();
-		view.getFrame().setVisible(true);
 	}
 
 }
