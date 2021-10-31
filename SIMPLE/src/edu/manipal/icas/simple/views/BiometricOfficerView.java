2e0b6b20dccea66cbc81fea962214a40b21b3c10
Yodhin Agarwal <71663803+Yodhin05@users.noreply.github.com>
2021-10-31 18:19:32 +0530

    Add files via upload

---
+++ SIMPLE/src/edu/manipal/icas/simple/views/BiometricOfficerView.java
@@ -0,0 +1,21 @@
+package edu.manipal.icas.simple.views;
+
+/**
+ * A view that enable the Biometrics Officer to scan and upload 
+ * applicants' biometrics. The scan is executed and uploaded with the help
+ * of buttons.
+ */
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+
+public interface BiometricOfficerView extends View{
+	
+	JButton getScanButton();
+	
+	JButton getSubmitScanButton();
+	
+	JTextField getApplicationIDTextField();
+}
