20ec87e622eb45d9b56ed5058b6f052face16357
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-11-04 15:08:40 +0530

    feat: Open current session from main if it exists

--- SIMPLE/src/edu/manipal/icas/simple/Main.java
+++ SIMPLE/src/edu/manipal/icas/simple/Main.java
@@ -1,12 +1,19 @@
 package edu.manipal.icas.simple;
 
 import edu.manipal.icas.simple.controllers.RouteController;
+import edu.manipal.icas.simple.controllers.SessionController;
+import edu.manipal.icas.simple.session.Session;
 import edu.manipal.icas.simple.controllers.Route;
 
 public class Main {
 
 	public static void main(String[] args) {
-		RouteController.getController().routeTo(Route.CITIZEN_LOGIN);
+		Session currentSession = SessionController.getController().getCurrentSession();
+		if(currentSession != null) {
+			RouteController.getController().routeTo(currentSession.getDefaultRoute());
+		} else {
+			RouteController.getController().routeTo(Route.CITIZEN_LOGIN);
+		}
 	}
 
 }
