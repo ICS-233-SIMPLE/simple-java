68f880dfb4495b9b26aa2f575b8f73fe0c16a428
Rea-Mammen <reamammen@outlook.com>
2021-11-06 02:05:55 +0530

    feat: Implement APIs for payment

---
+++ SIMPLE/src/edu/manipal/icas/simple/impl/apis/MockPaymentApi.java
@@ -0,0 +1,25 @@
+package edu.manipal.icas.simple.impl.apis;
+
+import edu.manipal.icas.simple.apis.PaymentApi;
+import edu.manipal.icas.simple.models.Payment;
+
+/**
+ * Dummy implementation of the Payment API.
+ * 
+ * @author Rea Mammen (rea.mammen@learner.manipal.edu)
+ *
+ */
+public class MockPaymentApi implements PaymentApi {
+
+	@Override
+	public Boolean processPayment(Payment payment) {
+		payment.fulfil();
+		return payment.getPaymentSuccessful();
+	}
+
+	@Override
+	public Payment createPayment(Double amount) {
+		return new Payment(amount);
+	}
+
+}
