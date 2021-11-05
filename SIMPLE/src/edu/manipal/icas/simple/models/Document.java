eee63e6fafe61913e1d0895708bd05b58022c767
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-11-05 12:00:51 +0530

    feat: Create document model

---
+++ SIMPLE/src/edu/manipal/icas/simple/models/Document.java
@@ -0,0 +1,36 @@
+package edu.manipal.icas.simple.models;
+
+import java.io.IOException;
+
+import edu.manipal.icas.simple.databases.DocumentDatabase;
+import edu.manipal.icas.simple.impl.databases.MsAccessDocumentDatabase;
+
+public class Document {
+	private String name;
+	private byte[] content;
+
+	private static final DocumentDatabase db = MsAccessDocumentDatabase.getDatabase();
+
+	public Document(String name, String path) {
+		if (db.documentExists(name)) {
+			throw new IllegalArgumentException("Document with the same name already exists!");
+		}
+		this.name = name;
+		this.content = null;
+		try {
+			db.createDocument(name, path);
+			this.content = db.fetchContent(name);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public byte[] getContent() {
+		return content;
+	}
+}
