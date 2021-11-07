6a4efb24fc42809826660a821d185962d9533f7c
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-11-08 03:38:44 +0530

    feat: Create passport model

--- SIMPLE/src/edu/manipal/icas/simple/models/Passport.java
+++ SIMPLE/src/edu/manipal/icas/simple/models/Passport.java
@@ -1,5 +1,6 @@
 package edu.manipal.icas.simple.models;
 
+import java.io.IOException;
 import java.util.Date;
 
 import edu.manipal.icas.simple.databases.PassportDatabase;
@@ -22,5 +23,100 @@ public class Passport {
 	private Date expiryDate;
 	private PassportStatus status;
 	private Citizen owner;
+
+	public Passport(String passportId) {
+		this.passportId = passportId;
+		init();
+	}
+
+	public Passport(Document photo, String birthAddress, String permanentAddress, String fathersName,
+			String mothersName, Date expiry, Citizen owner) {
+		try {
+			this.passportId = db.createPassport(photo.getName(), birthAddress, permanentAddress, fathersName, mothersName,
+					expiry, owner.getEmailAddress());
+			init();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void init() {
+		try {
+			photograph = new Document(db.fetchPhotographName(passportId));
+			birthAddress = db.fetchBirthAddress(passportId);
+			permanentAddress = db.fetchPermanentAddress(passportId);
+			nameOfFather = db.fetchNameOfFather(passportId);
+			nameOfMother = db.fetchNameOfMother(passportId);
+			expiryDate = db.fetchExpiryDate(passportId);
+			status = db.fetchStatus(passportId);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return the passportId
+	 */
+	public String getPassportId() {
+		return passportId;
+	}
+
+	/**
+	 * @return the photograph
+	 */
+	public Document getPhotograph() {
+		return photograph;
+	}
+
+	/**
+	 * @return the birthAddress
+	 */
+	public String getBirthAddress() {
+		return birthAddress;
+	}
+
+	/**
+	 * @return the permanentAddress
+	 */
+	public String getPermanentAddress() {
+		return permanentAddress;
+	}
+
+	/**
+	 * @return the nameOfFather
+	 */
+	public String getNameOfFather() {
+		return nameOfFather;
+	}
+
+	/**
+	 * @return the nameOfMother
+	 */
+	public String getNameOfMother() {
+		return nameOfMother;
+	}
+
+	/**
+	 * @return the expiryDate
+	 */
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public PassportStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * @return the owner
+	 */
+	public Citizen getOwner() {
+		return owner;
+	}
 
 }
