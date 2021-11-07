63d0576b4c76ed907c81aa6a5fa825fe189a822e
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-11-08 01:44:25 +0530

    feat(model): Add process application functionality to biometrics officer

--- SIMPLE/src/edu/manipal/icas/simple/models/BiometricsOfficer.java
+++ SIMPLE/src/edu/manipal/icas/simple/models/BiometricsOfficer.java
@@ -1,5 +1,10 @@
 package edu.manipal.icas.simple.models;
 
+import edu.manipal.icas.simple.apis.BiometricsCaptureApi;
+import edu.manipal.icas.simple.impl.apis.MockBiometricsCaptureApi;
+import edu.manipal.icas.simple.models.application.AcceptedBiometricType;
+import edu.manipal.icas.simple.models.application.Application;
+
 /**
  * Class that represents a biometrics officer. <br/>
  * The biometrics officer collects all the required biometrics from the citizen.
@@ -8,13 +13,26 @@ package edu.manipal.icas.simple.models;
  *
  */
 public class BiometricsOfficer extends PassportOfficer {
+	private BiometricsCaptureApi biometricsCaptureApi;
 
 	public BiometricsOfficer(Integer officerId) {
 		super(officerId);
+		biometricsCaptureApi = new MockBiometricsCaptureApi();
 	}
 
 	@Override
-	public void processApplication() {
+	public void processApplication(Application application) {
+		Document leftFingerprintDocument = biometricsCaptureApi.captureFingerprints();
+		application.uploadBiometric(AcceptedBiometricType.FINGERPRINTS_LEFT, leftFingerprintDocument);
+
+		Document rightFingerprintDocument = biometricsCaptureApi.captureFingerprints();
+		application.uploadBiometric(AcceptedBiometricType.FINGERPRINTS_RIGHT, rightFingerprintDocument);
+
+		Document thumbprints = biometricsCaptureApi.captureThumbPrints();
+		application.uploadBiometric(AcceptedBiometricType.THUMBPRINTS, thumbprints);
+
+		Document photograph = biometricsCaptureApi.capturePhotograph();
+		application.uploadBiometric(AcceptedBiometricType.PHOTOGRAPH, photograph);
 	}
 
 }
