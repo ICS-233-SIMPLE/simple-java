b27ededdf45934266bf46e2ad2c0d7989b342b72
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-11-07 13:05:54 +0530

    fix: Change passport office serialisation strategy

--- SIMPLE/src/edu/manipal/icas/simple/models/PassportOffice.java
+++ SIMPLE/src/edu/manipal/icas/simple/models/PassportOffice.java
@@ -6,34 +6,67 @@ import java.util.Date;
 import java.util.List;
 
 import edu.manipal.icas.simple.databases.PassportOfficeDatabase;
+import edu.manipal.icas.simple.databases.PassportOfficerDatabase;
 import edu.manipal.icas.simple.impl.databases.MsAccessPassportOfficeDatabase;
+import edu.manipal.icas.simple.impl.databases.MsAccessPassportOfficerDatabase;
 
 public class PassportOffice {
 	private Integer officeId;
 	private String officeAddress;
+	private List<PassportOfficer> officers;
 
 	private static final PassportOfficeDatabase db = MsAccessPassportOfficeDatabase.getDatabase();
+	private static final PassportOfficerDatabase pDb = MsAccessPassportOfficerDatabase.getDatabase();
+	private static List<PassportOffice> offices;
 
-	public static List<PassportOffice> getAllPassportOffices() {
-		return null;
+	static {
+		offices = new ArrayList<>();
+		for (int i = 1; i <= 16; ++i) {
+			offices.add(new PassportOffice(i));
+		}
 	}
 
-	public PassportOffice(Integer officeId) {
+	public static List<PassportOffice> getAllPassportOffices() {
+		return offices;
+	}
+
+	private PassportOffice(Integer officeId) {
 		this.officeId = officeId;
 		try {
 			officeAddress = db.fetchOfficeAddress(officeId);
+			List<Integer> officerIds = db.fetchOfficers(officeId);
+			officers = new ArrayList<>();
+			for (Integer id : officerIds) {
+				switch (pDb.fetchOfficerRole(officeId)) {
+				case VERIFICATION:
+					officers.add(new VerificationOfficer(officeId));
+					break;
+				case BIOMETRICS:
+					officers.add(new BiometricsOfficer(officeId));
+					break;
+				case GRANTING:
+					officers.add(new PassportGrantingOfficer(officeId));
+					break;
+				}
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	@SuppressWarnings("deprecation")
 	public List<Date> getAvailableSlots() {
 		try {
-			if (db.fetchAvailableSlots(officeId) == null) {
+			if (db.fetchAvailableSlots(officeId) == null || db.fetchAvailableSlots(officeId).isEmpty()) {
 				List<Date> slots = new ArrayList<>();
+				Date today = new Date(System.currentTimeMillis());
+				today.setHours(0);
+				today.setMinutes(0);
+				today.setSeconds(0);
 				for (int i = 0; i < 5; i++) {
-					slots.add(new Date(System.currentTimeMillis() + i * 3600 * 60 * 60 * 24));
+					slots.add(new Date(today.getTime()));
+					today = new Date(today.getTime() + 1000 * 60 * 60 * 24);
 				}
 				db.saveAvailableSlots(officeId, slots);
 				return slots;
@@ -55,12 +88,16 @@ public class PassportOffice {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getOfficeAddress() {
 		return officeAddress;
 	}
-	
+
 	public Integer getOfficeId() {
 		return officeId;
+	}
+
+	public List<PassportOfficer> getOfficers() {
+		return officers;
 	}
 }
