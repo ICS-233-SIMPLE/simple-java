37930a3148b29b156d0ecbdda368e08981bc5795
Vishwas-Adiga <vishwasadiga@gmail.com>
2021-11-08 01:44:04 +0530

    feat: Add getter for applications handled by an officer

--- SIMPLE/src/edu/manipal/icas/simple/impl/databases/MsAccessPassportOfficerDatabase.java
+++ SIMPLE/src/edu/manipal/icas/simple/impl/databases/MsAccessPassportOfficerDatabase.java
@@ -1,6 +1,12 @@
 package edu.manipal.icas.simple.impl.databases;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.healthmarketscience.jackcess.CursorBuilder;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
 
 import edu.manipal.icas.simple.databases.PassportOfficerDatabase;
 import edu.manipal.icas.simple.models.PassportOfficerRole;
@@ -31,6 +37,27 @@ public class MsAccessPassportOfficerDatabase extends MsAccessDatabase implements
 	@Override
 	public PassportOfficerRole fetchOfficerRole(Integer officerId) throws IOException {
 		return PassportOfficerRole.valueOf(getRow(officerId).getString(FIELD_OFFICER_ROLE));
+	}
+
+	@Override
+	public List<Integer> fetchApplications(Integer officerId) throws IOException {
+		List<Integer> applicationIds = new ArrayList<>();
+		
+		Table citizensTable = MsAccessApplicationDatabase.getDatabase().table;
+		Row row = null;
+		while((row = citizensTable.getNextRow()) != null) {
+			try {
+				String applicantId = row.getString(MsAccessApplicationDatabase.FIELD_APPLICANT_ID);
+				Integer officeId = MsAccessCitizenDatabase.getDatabase().fetchPassportOfficeId(applicantId);
+				if (MsAccessPassportOfficeDatabase.getDatabase().fetchOfficers(officeId).contains(officerId)) {
+					applicationIds.add(row.getInt(MsAccessApplicationDatabase.FIELD_ID));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+		}
+		return applicationIds;
 	}
 
 }
